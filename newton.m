6e21bee47e4927ab571a4dc5af1848008148dbc7
FrancoLeal <franco.leal@usach.cl>
2019-09-16 18:06:35 -0300

    comentarios

--- newton.m
+++ newton.m
@@ -4,16 +4,24 @@ disp("X0:");
 disp(x0)
 disp("iteracion:");
 disp(iterations);
-
+% Se evalúa x0 en la función
 fx0=polyval(fx,x0);
+% Se deriva la función
 df=polyder(fx);
+% Sse evalua x0 en la función derivada
 dfx0=polyval(df,x0);
+% Se calcula nuevo punto
 xk = x0-fx0/dfx0;
+Se evalua nuevo punto en la función
 fxk=polyval(fx,xk);
 
 
 disp("xk:");
 disp(xk);
+% Si el error es mayor que la función en el nuevo punto o si no quedan más
+% llamados recusrivos, se devuelve el nuevo punto
+% Sino, se vuelve a llamar el método con el nuevo punto como punto inicial
+% y se le resta 1 al contador de llamados.
 if(fxk<error || iterations==0)
     return;
 else
--- vector.m
+++ vector.m
@@ -1,7 +1,7 @@
 prompt = 'Ingrese un vector entre corchetes de al menos tamaÃ±o 4: ';
 
 x = [];
-
+% Se solicita un vector de tamaño y se verifica que al menos tenga tamaño 4
 while length(x) < 4
     try
         x = input(prompt);
@@ -9,16 +9,21 @@ while length(x) < 4
         disp('El vector ingresado no es valido')
     end
 end
-
+% Se ordenan los valores de menor a mayor
 x = sort(x);
-
+% Se obtienen los 4 elementos mayores
 mayores = x(end-3:end);
+% Se suman los 4 mayores
 sumaMayores = sum(mayores);
+% Se obtiene la raíz cuadrada de la suma de los 4 mayores
 raizSumaMayores = sqrt(sumaMayores);
-
+% Se  obtienen los 4 elementos menores
 menores = x(1:4);
+% Se calcula la raíz cuadrada de los 4 menores
 raizMenores = sqrt(menores);
+% Se suman las raíces de los 4 elementos menores
 sumaRaizMenores = sum(raizMenores);
-
+% Se restan la raíz de la suma de los 4 mayores con la suma de las raíces
+% de los 4 menores
 resta = raizSumaMayores - sumaRaizMenores
 
